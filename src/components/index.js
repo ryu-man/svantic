0cd37478aa380c2b7a4b62ea638722335ec3dc72
ryu-man <rioman19@gmail.com>
2021-01-26 00:07:18 +0100

    Update exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,41 +1,27 @@
 
-export { Breadcrumb, Section, Divider as BCDivider } from './breadcrumb'
-export { Button, Buttons } from './button'
-// export {
-//   Calendar,
-//   InlineCalendar,
-//   CalendarController
-// } from './calendar'
+export { Breadcrumb } from './breadcrumb'
+export { Button } from './button'
 export { Card } from './card'
-export { Form, Field, Fields } from './form'
-export { Header, H1, H2, H3, H4, H5 } from './header'
-export { Image, Link as ImageLink, Images } from './image'
+export { Form } from './form'
+export { Header } from './header'
+export { Image, Images } from './image'
 export { Input } from './input'
-export {
-  Label,
-  Ribbon,
-  Tag,
-  Labels
-} from './label'
-export { List, ListLink } from './list'
+export { Label, Labels } from './label'
+export { List } from './list'
 export { Item } from './item'
 export { Loader } from './loader'
-export { Menu, MenuItem } from './menu'
+export { Menu } from './menu'
 export { Message } from './message'
-export {
-  Placeholder,
-  Line,
-  Image as PlaceholderImage,
-  Paragraph,
-  Header as PlaceholderHeader
-} from './placeholder'
+export { Placeholder } from './placeholder'
 export { Rail } from './rail'
-export { Reveal, RevealContent } from './reveal'
-export { Segment } from './segment'
-export { Step, StepLink, Steps } from './step'
-export { Table } from './table'
-export { TabMenu, Tab, TabItem } from './tab'
-export { Text } from './text'
-export { Icon, Icons } from './icon'
-export { Statistic, Statistics } from './statistic'
+export { Reveal } from './reveal'
+export * from './segment'
+export * from './step'
+export * from './table'
+export * from './text'
+export * from './icon'
+export * from './statistic'
 export { Divider } from './divider'
+export * from './grid'
+export * from './container'
+export { Shape } from './shape'
