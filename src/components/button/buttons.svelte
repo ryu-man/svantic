e0326ff38b409199c0d08082304c503b8b42ad24
ryu-man <rioman19@gmail.com>
2021-01-25 23:29:19 +0100

    Fix component formatting style

--- src/components/button/buttons.svelte
+++ src/components/button/buttons.svelte
@@ -3,13 +3,11 @@
   import { css } from '../../utils'
 
   export let color = ''
-  export let style= {}
+  export let style = {}
   let _class = ''
   export { _class as class }
 </script>
 
 <div use:css="{style}" class="{_class} {color} ui buttons">
-  <slot>
-    <!-- optional fallback -->
-  </slot>
+  <slot />
 </div>
