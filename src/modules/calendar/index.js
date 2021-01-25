27bfbff5a101bdc04d0628ccfbc6a8ef1daa3637
ryu-man <rioman19@gmail.com>
2021-01-25 23:57:15 +0100

    Rename variable name

--- src/modules/calendar/index.js
+++ src/modules/calendar/index.js
@@ -2,7 +2,7 @@ import { default as Calendar } from './calendar.svelte'
 import { default as InlineCalendar } from './inline_calendar.svelte'
 import { default as CalendarController } from './controller'
 
-Calendar.inline = InlineCalendar
+Calendar.Inline = InlineCalendar
 
 export { Calendar, InlineCalendar, CalendarController }
 export default Calendar
