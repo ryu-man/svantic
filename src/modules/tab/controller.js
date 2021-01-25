22270af29f9aa3d2761262713a810478b91f05db
ryu-man <rioman19@gmail.com>
2021-01-25 23:56:36 +0100

    Add tab module

---
+++ src/modules/tab/controller.js
@@ -0,0 +1,50 @@
+export default class TabularController {
+    constructor(target) {
+        this.jQuery = window['$'] || window['jQuery'];
+        if (!this.jQuery)
+            throw Error('jQuery not found, make sure you include jquery in the head tag');
+        this.target = target;
+        this.selection = this.jQuery(target);
+    }
+    init() {
+        this.selection.toast(this.settings);
+        return this;
+    }
+    setSettings(settings) {
+        this.settings = settings;
+        return this;
+    }
+    customBehavior(behavior, ...args) {
+        return this.selection.toast(behavior, ...args);
+    }
+    /**
+     *@description 	Pauses the display time decrease (and possible progress bar animation)
+     */
+    animatePause() {
+        this.customBehavior('animate pause');
+    }
+    /**
+     *@description 	Continues decreasing display time (and possible progress bar animation)
+     */
+    animateContinue() {
+        this.customBehavior('animate continue');
+    }
+    /**
+     *@description 	Closes the toast
+     */
+    close() {
+        this.customBehavior('close');
+    }
+    /**
+     *@description 	Returns all toasts as an array of objects which are visible within the current toast-container
+     */
+    getToasts() {
+        return this.customBehavior('get toasts');
+    }
+    /**
+     *@description 	Returns the remaining time in milliseconds
+     */
+    getRemainingTime() {
+        return this.customBehavior('get remainingTime');
+    }
+}
---
+++ src/modules/tab/index.js
@@ -0,0 +1,10 @@
+import { default as TabMenu } from './tabular_menu.svelte'
+import { default as Tab } from './tab.svelte'
+import { default as Item } from './item.svelte'
+import {default as TabController} from './controller'
+
+TabMenu.Tab = Tab
+TabMenu.Item = Item
+
+export { TabMenu, Tab, Item, TabController }
+export default TabMenu
