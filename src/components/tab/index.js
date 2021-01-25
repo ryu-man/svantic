78ee09741aa5595284d5bbd676284759e16f4911
ryu-man <rioman19@gmail.com>
2021-01-26 00:09:27 +0100

    Delete tab module from components folder

--- src/components/tab/index.js
+++
@@ -1,8 +0,0 @@
-import { default as TabMenu } from './tabular_menu.svelte'
-import { default as Tab } from './tab.svelte'
-import { default as TabItem } from './item.svelte'
-
-TabMenu.tab = Tab
-TabMenu.item = TabItem
-
-export { TabMenu, Tab, TabItem }
