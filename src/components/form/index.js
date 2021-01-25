2a0dbc63c6c817dbaa8f49d23a88a8b7a5ae5d7b
ryu-man <rioman19@gmail.com>
2021-01-25 23:41:22 +0100

    Update variables names

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,8 +2,8 @@ import { default as Form } from './form.svelte'
 import { default as Field } from './field.svelte'
 import { default as Fields } from './fields.svelte'
 
- Form.field = Field
- Form.fields = Fields
+ Form.Field = Field
+ Form.Fields = Fields
 
  export {Form, Field, Fields}
  export default Form
