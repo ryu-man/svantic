c91160a135d6f9353f85e04318d90d13b52d473a
ryu-man <rioman19@gmail.com>
2021-02-01 12:00:19 +0100

    refactor: fix exports & add sub header

--- src/elements/header/index.d.ts
+++ src/elements/header/index.d.ts
@@ -1,6 +1,10 @@
-export { default as Header } from "./header";
-export { default as H1 } from "./h1";
-export { default as H2 } from "./h2";
-export { default as H3 } from "./h3";
-export { default as H4 } from "./h4";
-export { default as H5 } from "./h5";
+import { default as Header } from './header.svelte'
+import { default as H1 } from './h1.svelte'
+import { default as H2 } from './h2.svelte'
+import { default as H3 } from './h3.svelte'
+import { default as H4 } from './h4.svelte'
+import { default as H5 } from './h5.svelte'
+import {default as Sub} from './sub.svelte'
+
+export { Header, H1, H2, H3, H4, H5, Sub }
+export default Header
--- src/elements/header/index.js
+++ src/elements/header/index.js
@@ -6,12 +6,14 @@ import { default as H2 } from './h2.svelte'
 import { default as H3 } from './h3.svelte'
 import { default as H4 } from './h4.svelte'
 import { default as H5 } from './h5.svelte'
+import {default as Sub} from './sub.svelte'
 
 Header.H1 = H1
 Header.H2 = H2
 Header.H3 = H3
 Header.H4 = H4
 Header.H5 = H5
+Header.Sub = Sub
 
-export { Header, H1, H2, H3, H4, H5 }
+export { Header, H1, H2, H3, H4, H5, Sub }
 export default Header
