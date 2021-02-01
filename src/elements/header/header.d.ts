daa164070cdde0630c5c27557cb4bc0a89b3352d
ryu-man <rioman19@gmail.com>
2021-02-01 11:57:18 +0100

    refactor: Add h1...h5 & sub header

--- src/elements/header/header.d.ts
+++ src/elements/header/header.d.ts
@@ -1,28 +1,39 @@
-import { SvelteComponentTyped} from 'svelte/internal';
-import { Style } from '../../style';
-import { Component } from '../../component';
+import { SvelteComponentTyped } from 'svelte/internal'
+import { Component } from '../../component'
+import { default as H1 } from './h1'
+import { default as H2 } from './h2'
+import { default as H3 } from './h3'
+import { default as H4 } from './h4'
+import { default as H5 } from './h5'
+import type Sub from './sub'
+
 interface HeaderProps extends Component {
-    class?: string;
-    active?: boolean;
-    disabled?: boolean;
-    fluid?: boolean;
-    circular?: boolean;
-    loading?: boolean;
-    primary?: boolean;
-    secondary?: boolean;
-    compact?: boolean;
-    basic?: boolean;
-    toggle?: boolean;
-    positive?: boolean;
-    negative?: boolean;
-    tertiary?: boolean;
-    inverted?: boolean;
-    icon?: boolean;
-    style?: Style;
+  active?: boolean
+  disabled?: boolean
+  fluid?: boolean
+  circular?: boolean
+  loading?: boolean
+  primary?: boolean
+  secondary?: boolean
+  compact?: boolean
+  basic?: boolean
+  toggle?: boolean
+  positive?: boolean
+  negative?: boolean
+  tertiary?: boolean
+  inverted?: boolean
+  icon?: boolean
+  sub?: boolean
 }
 /**
- * Vomantic Header
+ * @description
  */
 declare class Header extends SvelteComponentTyped<HeaderProps> {
+static H1 : new ()=> H1
+static H2 : new ()=> H2
+static H3 : new ()=> H3
+static H4 : new ()=> H4
+static H5 : new ()=> H5
+static Sub : new ()=> Sub
 }
-export default Header;
+export default Header
