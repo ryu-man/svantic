741127f80fd169a5dbd5680570f2e16dc514f9ad
ryu-man <rioman19@gmail.com>
2021-02-04 22:18:45 +0100

    refactor: Update props

--- src/elements/button/buttons.d.ts
+++ src/elements/button/buttons.d.ts
@@ -1,12 +1,17 @@
-import { SvelteComponentTyped} from 'svelte/internal';
-import type { Component } from '../../component';
-import type { Color } from '../../variations';
+import { SvelteComponentTyped } from 'svelte/internal'
+import type { Component } from '../../component'
+import type { Color, Float, Size, Wide } from '../../variations'
 interface ButtonsProps extends Component {
-    color?: Color;
+  color?: Color
+  icon?: boolean
+  vertical?: boolean
+  labeled?: boolean|Float
+  basic?: boolean
+  size?: Size
+  width?: Wide
 }
 /**
  * Vomantic Buttons
  */
-declare class Buttons extends SvelteComponentTyped<ButtonsProps> {
-}
-export default Buttons;
+declare class Buttons extends SvelteComponentTyped<ButtonsProps> {}
+export default Buttons
--- src/elements/button/buttons.svelte
+++ src/elements/button/buttons.svelte
@@ -1,13 +1,21 @@
 <script>
-  import '../../../semantic/dist/components/button.css'
-  import { css } from '../../utils'
+  import { css, classNames } from '../../utils'
 
-  export let color = ''
-  export let style = {}
   let _class = ''
   export { _class as class }
+  export let color = ''
+  export let icon = false
+  export let vertical = false
+  export let labeled
+  export let basic = false
+  export let size
+  export let width
+  export let style = {}
 </script>
 
-<div use:css="{style}" class="{_class} {color} ui buttons">
+<div
+  use:css="{style}"
+  class="{classNames('ui', [labeled, 'labeled'], { icon, vertical, basic }, color, size, width, _class, 'buttons')}"
+>
   <slot />
 </div>
