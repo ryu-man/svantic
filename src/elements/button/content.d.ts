8d240f7ecc7924d53826915e607e0252d65d69a6
ryu-man <rioman19@gmail.com>
2021-02-04 22:17:40 +0100

    feat: Create animated content

---
+++ src/elements/button/content.d.ts
@@ -0,0 +1,11 @@
+import { SvelteComponentTyped } from 'svelte/internal'
+
+interface ContentProps {
+  hidden?: boolean
+}
+
+/**
+ * @description
+ */
+declare class Content extends SvelteComponentTyped<ContentProps> {}
+export default Content
---
+++ src/elements/button/content.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let hidden = false
+</script>
+
+<div class:visible="{!hidden}" class:hidden class="content">
+  <slot />
+</div>
