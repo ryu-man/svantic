64f25a078b251b269f40a52c51329e888849fb9e
ryu-man <rioman19@gmail.com>
2021-02-06 00:26:09 +0100

    feat: Create emoji module

---
+++ src/elements/emoji/emoji.svelte
@@ -0,0 +1,19 @@
+<script>
+  import '../../../semantic/dist/components/emoji.min.css'
+  import { classNames } from '../../utils'
+
+  let _class = ''
+  export { _class as class }
+  export let name
+  export let disabled = false
+  export let loading = false
+  export let size
+  export let link = false
+</script>
+
+<em
+  data-emoji="{name}"
+  class:disabled
+  class:loading
+  class:link
+  class="{classNames(size, _class)}"></em>
