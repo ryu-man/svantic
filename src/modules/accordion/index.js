7ba7e9c355d090a43809aa5d559e7a70a6f79eef
ryu-man <rioman19@gmail.com>
2021-02-08 23:09:45 +0100

    refactor: Rename child class

--- src/modules/accordion/index.js
+++ src/modules/accordion/index.js
@@ -3,8 +3,8 @@ import { default as Accordion } from './accordion.svelte'
 import { default as Title } from './title.svelte'
 import { default as Content } from './content.svelte'
 
-Accordion.Title = Title
-Accordion.Content = Content
+Accordion.title = Title
+Accordion.content = Content
 
 export { Accordion, AccordionController, Title, Content }
 export default Accordion
