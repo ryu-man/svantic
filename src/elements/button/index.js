b4f21bed37e6e7a513511f5684ba9e95ff4b7e2d
ryu-man <rioman19@gmail.com>
2021-02-08 23:18:15 +0100

    refactor: Rename child constructor

--- src/elements/button/index.js
+++ src/elements/button/index.js
@@ -3,8 +3,8 @@ import { default as Button } from "./button.svelte";
 import { default as Content } from "./content.svelte"
 import { default as Or} from './or.svelte'
 
-Button.Content = Content
-Button.Or = Or
+Button.content = Content
+Button.or = Or
 
 export { Button, Buttons , Content}
 export default Button
