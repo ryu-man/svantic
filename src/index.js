5c207ad310a9b0763ce702d8f2045b9933792030
ryu-man <rioman19@gmail.com>
2021-02-09 22:59:48 +0100

    refactor: remove css imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import '../semantic/dist/components/site.min.css'
-import '../semantic/dist/components/reset.min.css'
-import '../semantic/dist/components/transition.min.css'
-import '../semantic/dist/components/dimmer.min.css'
-import '../semantic/dist/components/emoji.min.css'
-import '../semantic/dist/components/nag.min.css'
-
 export * from './collections'
 export * from './elements'
 export * from './modules'
