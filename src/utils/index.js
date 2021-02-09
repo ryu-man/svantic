5be85b061964923601157cd237fd48c81f2b9377
ryu-man <rioman19@gmail.com>
2021-02-09 22:59:27 +0100

    refactor: Enhance functionality

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,39 @@
-export function css(node, style) {
-    const stylesheet = node.style;
-    Object.entries(style).forEach((elem) => {
-        stylesheet[elem[0]] = elem[1];
-    });
+export function css(node, style = {}) {
+  const stylesheet = node.style
+  Object.entries(style).forEach((elem) => {
+    stylesheet[elem[0]] = elem[1]
+  })
 }
 export function register(node, events) {
-    const eventTypes = Object.keys(events);
-    eventTypes.forEach((key) => node.addEventListener(key, events[key]));
-    return () => {
-        eventTypes.forEach((key) => node.removeEventListener(key, events[key]));
-    };
+  const eventTypes = Object.keys(events)
+  eventTypes.forEach((key) => node.addEventListener(key, events[key]))
+  return () => {
+    eventTypes.forEach((key) => node.removeEventListener(key, events[key]))
+  }
 }
+
 export function classNames(...args) {
-    return args.filter((o) => o).map(o => {
-        if (Array.isArray(o)) {
-            const [arg1, arg2] = o
-            if (arg1) {
-                if (typeof arg1 === typeof true) {
-                    return arg2
-                }
-                return `${arg1} ${arg2}`
-            } else {
-                return ''
+  return args
+    .filter((o) => o)
+    .map((o) => {
+      if (typeof o === 'string') {
+        return o.trim()
+      }
+      return Array.from(Object.entries(o))
+        .map(([e1, e2]) => {
+          if (e2) {
+            if (typeof e2 === 'boolean') {
+              return e1
             }
-        }
-        return o
-    }).join(' ');
+            return `${e2?.trim()} ${e1}`
+          }
+          return ''
+        })
+        .filter((o) => o)
+        .join(' ')
+        .trim()
+    })
+    .filter((o) => o)
+    .join(' ')
+    .trim()
 }
