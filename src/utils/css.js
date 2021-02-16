4c4dceb4a99da5af01a7a82e38fb50a938600a1c
ryu-man <rioman19@gmail.com>
2021-02-16 22:10:24 +0100

    Delete unecessairy files

--- src/utils/css.js
+++
@@ -1,6 +0,0 @@
-export function css(node, style) {
-    const stylesheet = node.style;
-    Object.entries(style).forEach((elem) => {
-        stylesheet[elem[0]] = elem[1];
-    });
-}
--- src/utils/events.js
+++
@@ -1,7 +0,0 @@
-export function register(node, events) {
-    const eventKeys = Object.keys(events);
-    eventKeys.forEach((key) => node.addEventListener(key, events[key]));
-    return () => {
-        eventKeys.forEach((key) => node.removeEventListener(key, events[key]));
-    };
-}
