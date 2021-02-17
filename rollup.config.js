2be0d7338787bc51593a3becd130bbec4c3f523a
ryu-man <rioman19@gmail.com>
2021-02-17 17:46:45 +0100

    Fix build errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,88 +1,97 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from "svelte-preprocess";
-import typescript from '@rollup/plugin-typescript'
-import css from "rollup-plugin-css-only"
-const pkg = require('./package.json');
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
+import sveltePreprocess from 'svelte-preprocess'
+import css from 'rollup-plugin-css-only'
+const pkg = require('./package.json')
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 const { name } = pkg
-const globals = {
-	sveltic: "svantic"
-}
+
 function serve() {
-	let server;
+  let server
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0)
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return
+      server = require('child_process').spawn(
+        'npm',
+        ['run', 'start', '--', '--dev'],
+        {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true
+        }
+      )
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on('SIGTERM', toExit)
+      process.on('exit', toExit)
+    }
+  }
 }
 
 export default {
-	input: './src/index.js',
-	output: [
-		{ file: pkg.module, format: 'es', globals},
-		{ file: pkg.main, format: 'umd', name, globals}
-	],
-	inlineDynamicImports: true,
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
+  input: './src/index.js',
+  output: [
+    {
+      dir: 'dist/module',
+      sourcemap: true,
+      format: 'es',
+      entryFileNames: 'index.js',
+      chunkFileNames: '[name].js'
+    },
+    {
+      dir: 'dist/nomodule',
+      sourcemap: true,
+      format: 'system',
+      name: pkg.name,
+      entryFileNames: 'index.js',
+      chunkFileNames: '[name].js'
+    }
+  ],
 
-			compilerOptions: {
-				dev: !production,
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
 
-			},
-			emitCss:false,
-			// preprocess: sveltePreprocess({}),
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		// css({ output: 'dist/bundle.css' }),
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			dedupe: ['svelte'],
-			extensions: [".js", ".ts"]
-		}),
-		commonjs(),
-		typescript({ sourceMap: !production }),
+      compilerOptions: {
+        dev: !production
+      },
+      emitCss: false,
+      preprocess: sveltePreprocess({})
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: 'bundle.css'}),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      dedupe: ['svelte'],
+      extensions: ['.js']
+    }),
+    commonjs(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: true
-	},
-	external: [
-		"fomantic-ui"
-	]
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: true
+  },
+  external: ['fomantic-ui', 'jquery']
 }
