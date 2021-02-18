c0467b8336b03426e299bb802bf5b85f12fcb091
ryu-man <rioman19@gmail.com>
2021-02-18 09:48:01 +0100

    Clean up unused files

--- src/elements/event_map.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type EventMap<T extends keyof HTMLElementEventMap, R> = {
-    [key in T]?: R;
-};
--- src/elements/type.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Attached = 'top' | 'bottom';
-export declare type Floated = 'left' | 'right';
-export declare type Aligned = 'left' | 'center' | 'right';
-export declare type Size = 'tiny' | 'small' | 'large' | 'big';
-export declare type Speed = 'slow' | 'fast';
-export declare type Color = 'red' | 'orange' | 'teal' | 'blue' | 'violet' | 'purple' | 'pink' | 'brown' | 'grey' | 'black';
