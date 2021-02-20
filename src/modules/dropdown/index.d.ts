fb9862b8270ac9ba0cddf51a95d81accf89fceb0
ryu-man <rioman19@gmail.com>
2021-02-20 22:57:59 +0100

    refactor: Update exports

--- src/modules/dropdown/index.d.ts
+++ src/modules/dropdown/index.d.ts
@@ -1,6 +1,11 @@
 export { default as DropdownController } from './controller'
 import { default as Dropdown } from './dropdown'
 import { default as Select } from './select'
+import Header from './header'
+import Menu from './menu'
+import Item from './item'
+import Divider from './divider'
+import Text from './text'
 
-export { Dropdown, Select }
+export { Dropdown, Select, Header, Divider, Menu, Item, Text }
 export default Dropdown
--- src/modules/dropdown/index.js
+++ src/modules/dropdown/index.js
@@ -1,8 +1,19 @@
 export { default as DropdownController } from './controller'
 import { default as Dropdown } from './dropdown.svelte'
 import { default as Select } from './select.svelte'
+import Header from './header.svelte'
+import Menu from './menu.svelte'
+import Item from './item.svelte'
+import Divider from './divider.svelte'
+import Text from './text.svelte'
 
 Dropdown.select = Select
+Dropdown.select = Dropdown
+Dropdown.header = Header
+Dropdown.divider = Divider
+Dropdown.menu = Menu
+Dropdown.item = Item
+Dropdown.text = Text
 
-export { Dropdown, Select }
+export { Dropdown, Select, Header, Divider, Menu, Item, Text }
 export default Dropdown
