ec2915e31ffbd8fb4074968de00bb0562ad7b4d0
ryu-man <rioman19@gmail.com>
2021-02-20 10:36:39 +0100

    feat: Add Table row

---
+++ src/collections/table/row.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { css } from '../../utils'
+    let _class = ''
+    export { _class as class }
+    export let style
+  </script>
+  
+  <tr use:css="{style}" class="{_class}">
+    <slot />
+  </tr>
