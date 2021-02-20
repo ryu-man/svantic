c3e47ddf3592b3e0a2da3a6fccb2e3fd0cd042b6
ryu-man <rioman19@gmail.com>
2021-02-20 22:52:37 +0100

    feat: Add dropdown divider

---
+++ src/modules/dropdown/divider.d.ts
@@ -0,0 +1,11 @@
+import { SvelteComponentTyped } from 'svelte/internal'
+import type { Component } from 'src/component'
+import type { Icons } from 'src/elements/icon/icon'
+
+export interface DropdownDividerProps extends Component {
+}
+/**
+ * Svantic Dropdown Divider
+ */
+declare class DropdownDivider extends SvelteComponentTyped<DropdownDividerProps> {}
+export default DropdownDivider
---
+++ src/modules/dropdown/divider.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {css, classNames} from '../../utils'
+    let _class
+    export {_class as class}
+    export let style
+</script>
+
+<div use:css={style} class="{classNames(_class,"divider")}"></div>
