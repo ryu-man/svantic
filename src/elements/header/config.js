b77863df138072f4f76132cbbebcd415cd9931b0
ryu-man <rioman19@gmail.com>
2021-02-26 16:06:17 +0100

    refactor: Move out side effects to a seperate file

--- src/elements/header/config.js
+++ src/elements/header/config.js
@@ -5,7 +5,10 @@ import { default as H3 } from './h3.svelte'
 import { default as H4 } from './h4.svelte'
 import { default as H5 } from './h5.svelte'
 import { default as Sub } from './sub.svelte'
-import './config'
 
-export { Header, H1, H2, H3, H4, H5, Sub }
-export default Header
+Header.h1 = H1
+Header.h2 = H2
+Header.h3 = H3
+Header.h4 = H4
+Header.h5 = H5
+Header.sub = Sub
