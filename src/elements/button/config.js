42f694debceb6caebcd6baa09790dc53e1c04e64
ryu-man <rioman19@gmail.com>
2021-02-26 16:07:02 +0100

    refactor: Move out side effects to seperate file

--- src/elements/button/config.js
+++ src/elements/button/config.js
@@ -1,8 +1,6 @@
-import { default as Buttons } from "./buttons.svelte";
 import { default as Button } from "./button.svelte";
 import { default as Content } from "./content.svelte"
 import { default as Or} from './or.svelte'
-import './config'
 
-export { Button, Buttons , Content, Or}
-export default Button
+Button.content = Content
+Button.or = Or
--- src/elements/button/index.js
+++ src/elements/button/index.js
@@ -2,9 +2,7 @@ import { default as Buttons } from "./buttons.svelte";
 import { default as Button } from "./button.svelte";
 import { default as Content } from "./content.svelte"
 import { default as Or} from './or.svelte'
+import './config'
 
-Button.content = Content
-Button.or = Or
-
-export { Button, Buttons , Content}
+export { Button, Buttons , Content, Or}
 export default Button
